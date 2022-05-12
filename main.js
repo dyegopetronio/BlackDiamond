fb7212735fef78203f6aae7c54e06cf1e41cb7b4
Dyego Petronio <dyegopetronio@gmail.com>
2022-05-11 21:31:36 -0300

    Botão BackToTop

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScrool()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScrool() {
+  if (scrollY > 525) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
-# Black Diamond
+# Observação
+
+Aplicação sendo criada de acordo com fluxo das aulas.
+
+## Black Diamond
 
 > Site de uma joalheria
 
-## Descriçao
+### Descriçao
 
 Aplicaçao desenvolvida durante o evento NLW Origin da Rocketseat, com a orientaçao do professor Mayk Brito.
